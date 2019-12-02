7946a16801af69a779b9083060de2adc8912aef6
alfpml <alfpml@gmail.com>
2019-12-02 13:45:10 +0100

    commit

--- api.py
+++ api.py
@@ -12,21 +12,21 @@ def index():
 
 @get("/<chat_id>/messages")
 def getMessages(chat_id):
-    """You can get the list of messages from the chat you select"""
-    return dumps(coll.find({'idChat':int(chat_id)}, {"userName":1,"text": 1,"_id":0}))
+    """Get messages from a given chat"""
+    return dumps(coll.find({'idChat':int(chat_id)}))
     
 
 @get("/users")
 def getUsers():
-    """You can get the list of all the users of the DB with their user_id associated"""
+    """Get all users"""
     return dumps(coll.aggregate([{"$group":{"_id": {"idUser":"$idUser", "userName":"$userName"}}}]))
 
 @post('/user/create')
 def createuser():
-    """You can create a new user to upload to the DB"""
+    """New user creation"""
     name = str(request.forms.get("name"))
     new_id = coll.distinct("idUser")[-1] + 1
-    new_user = {
+
         "idUser":new_id,
         "userName":name
     }
