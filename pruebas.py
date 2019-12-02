21e68026bbc62111886717a6ff6ba155d48e988d
alfpml <alfpml@gmail.com>
2019-12-02 11:46:56 +0100

    c

--- pruebas.py
+++ pruebas.py
@@ -3,6 +3,12 @@ import getpass
 import json
 import mongofnc as mf
 
+#Get Password
+password = getpass.getpass("Insert your AtlasMongoDB admin_1019 password: ")
+connection = "mongodb+srv://alfpml:{}@cluster0-c3fib.mongodb.net/test?retryWrites=true&w=majority".format(password)
+#Connect to DB
+client = MongoClient(connection)
+
 db, coll = mf.connectCollection('chats','chatprueba')
 
 with open('input/chats.json') as f:
